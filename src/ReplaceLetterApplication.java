c4f48f0e64af76f0521b61616e705a8c33a3170a
Mehran <36104315+Mehran-at@users.noreply.github.com>
2019-04-02 12:24:48 -0400

    Update ReplaceLetterApplication.java

--- src/ReplaceLetterApplication.java
+++ src/ReplaceLetterApplication.java
@@ -1,7 +1,16 @@
+// Develop the following code:
+//      a. Define the replaceAll action that receives a string and two letters. It will replace the first letter with the second letter every time it appears in the string. Then it returns this modified string.
+//      b. Define a string.
+//      c. Write a code that replaces every occurrence of the letter e with the letter o and says the new string.
+//  Available actions: getTextDocument()
+
+// Text example: “There was a terrible noise”
+// Text replacement: “Thoro was a torriblo noiso”
+
 public class ReplaceLetterApplication {
     public static void main(String[] args) {
         Text text = new Text();
-        String textContent = "Java String replace(char old, char new) method example";
-        System.out.println(text.replaceAll(textContent ,"a", "b"));
+        String textContent = "Java String replace(char old, char new) method";
+        System.out.println(text.replaceAll(textContent ,"e", "o"));
     }
 }
